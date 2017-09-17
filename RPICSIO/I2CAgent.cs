08a4f1e6557803b8a4c9ffbdebd342cfa8897ff2
Thomas Jam Pedersen <thomas@etjam.dk>
2017-09-17 22:39:10 +0200

    First commit, downloaded from http://www.ofitselfso.com/RPICSIO/RPICSIOSource_0100.zip, made few minor changes to test it on RPi 3

---
+++ RPICSIO/I2CAgent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace RPICSIO
+{
+    public class I2CAgent
+    {
+        private I2CPortFS _port;
+        private int _address;
+
+        public I2CAgent(I2CPortFS port, int address)
+        {
+            _port = port;
+            _address = address;
+
+            if (_port.PortIsOpen == false)
+            {
+                throw new Exception("ssHandle == null");
+            }
+        }
+
+        public byte[] Read(int bytes)
+        {
+            var buffer = new byte[bytes];
+            _port.Read(_address, buffer, bytes);
+            return buffer;
+        }
+
+        public byte ReadByte()
+        {
+            return Read(1).First();
+        }
+
+        public void Write(byte[] buffer)
+        {
+            _port.Write(_address, buffer, buffer.Length);
+        }
+
+        public void WriteByte(byte value)
+        {
+            Write(new[] { value });
+        }
+    }
+}
